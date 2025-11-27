af91561196eff2259908135bb0427f895851fd33
fxdecod-sys <f.x.decod@gmail.com>
2025-11-27 12:36:02 +0300

    Delete components directory

--- components/Spinner.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const Spinner: React.FC = () => (
-  <div className="flex justify-center items-center">
-    <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-emerald-500"></div>
-  </div>
-);
